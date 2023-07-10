3812048c1820b5b8f01a1780675298b173bc4d3c
Julius Shipton <juliusshipton@gmail.com>
2023-07-10 12:25:18 +0800

    initial commit

---
+++ python2/main.py
@@ -0,0 +1,6 @@
+
+from TimeTagger import TimeTagger
+
+
+for i in range(1, 10):
+	print "Python 2 running ..."
---
+++ python3/main.py
@@ -0,0 +1,6 @@
+
+
+from hardware.nidaq import Scanner
+
+for i in range(1, 10):
+    print("Python 3 running ...")
