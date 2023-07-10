692fc90f089cbaf8062a9a620f12917d0f73b9f8
Julius Shipton <juliusshipton@gmail.com>
2023-07-10 16:37:37 +0800

    Added simple socket echo  between python versions

--- python2/main.py
+++ python2/main.py
@@ -1,7 +1,36 @@
 
 # from TimeTagger import TimeTagger
 
-from PulseGenerator.pulse_generator import PulseGenerator as PulseGeneratorBase
+# from PulseGenerator.pulse_generator import PulseGenerator as PulseGeneratorBase
 
-for i in range(1, 10):
-	print "Python 2 running ..."
+import socket
+import sys
+
+print "Python 2 running ..."
+
+HOST = '127.0.0.1' 
+PORT = 8888
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+conn, addr = s.accept()
+
+print "Listening on port" + str(PORT)
+
+while True: 
+	
+	# String data received from client
+	data = conn.recv(1024).decode()
+	
+	# Display and manipulate
+	print data
+	data = data.upper()
+	
+	# Return
+	conn.sendall(data)
+
+	# Kill check
+	if data == 'EXIT':
+		s.close()
+		sys.exit()
