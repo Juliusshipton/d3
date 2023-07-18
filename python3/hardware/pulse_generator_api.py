1d78cd2b321fd11e2f49e5b58bbab73a72fa8f1a
Julius Shipton <juliusshipton@gmail.com>
2023-07-18 16:39:04 +0800

    changed pg constructor

--- python3/hardware/pulse_generator_api.py
+++ python3/hardware/pulse_generator_api.py
@@ -6,7 +6,7 @@ This will be the PulseGeneratorApi that will talk to python 2 pulse generator in
 
 class PulseGenerator():
 	
-	def __init__(self, serial='', channel_map={'ch0':0,'ch1':1,'ch2':2,'ch3':3,'ch4':4,'ch5':5,'ch6':6,'ch7':7,'ch8':8,'ch9':9,'ch10':10,'ch11':11,'ch12':12,'ch13':13,'ch14':14,'ch15':15,'ch16':16,'ch17':17,'ch18':18,'ch19':19,'ch20':20,'ch21':21,'ch22':22,'ch23':23}, core='12x8'):
+	def __init__(self, serial, channel_map={'ch0':0,'ch1':1,'ch2':2,'ch3':3,'ch4':4,'ch5':5,'ch6':6,'ch7':7,'ch8':8,'ch9':9,'ch10':10,'ch11':11,'ch12':12,'ch13':13,'ch14':14,'ch15':15,'ch16':16,'ch17':17,'ch18':18,'ch19':19,'ch20':20,'ch21':21,'ch22':22,'ch23':23}, core='12x8'):
 		self.serial = serial
 		self.channel_map = channel_map
 		self.xem = ok.FrontPanel()
