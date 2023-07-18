d874947708155efa7e961ff8cf6ee77852860971
Julius Shipton <juliusshipton@gmail.com>
2023-07-18 15:18:18 +0800

    commented out nidac call only focusing on imports

--- python3/hardware/api.py
+++ python3/hardware/api.py
@@ -115,14 +115,16 @@ from tools.utility import singleton
 @singleton
 def Scanner():
 	from .nidaq import Scanner
-	return Scanner( CounterIn='/Dev1/Ctr1',
-					CounterOut='/Dev1/Ctr0',
-					TickSource='/Dev1/PFI3',
-					AOChannels='/Dev1/ao0:2',
-					x_range=(0.0,200.0),#100x
-					y_range=(0.0,200.0),
-					z_range=(0,100.0),
-					v_range=(-1.00,1.00))
+	
+	# PYTHON3 EDIT Uncomment when NIDAC plugged in
+	# return Scanner( CounterIn='/Dev1/Ctr1',
+	# 				CounterOut='/Dev1/Ctr0',
+	# 				TickSource='/Dev1/PFI3',
+	# 				AOChannels='/Dev1/ao0:2',
+	# 				x_range=(0.0,200.0),#100x
+	# 				y_range=(0.0,200.0),
+	# 				z_range=(0,100.0),
+	# 				v_range=(-1.00,1.00))
 
 ScnnerA=Scanner
 
