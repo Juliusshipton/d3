40aa2c41cf012621b5e7788bd84c2605a0b26f59
Julius Shipton <juliusshipton@gmail.com>
2023-07-24 15:26:55 +0800

    removed socket stuff

--- python2/main.py
+++ python2/main.py
@@ -8,15 +8,15 @@ import sys
 
 print "Python 2 running ..."
 
-HOST = '127.0.0.1' 
-PORT = 8888
+# HOST = '127.0.0.1' 
+# PORT = 8888
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(5)
-conn, addr = s.accept()
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind((HOST, PORT))
+# s.listen(5)
+# conn, addr = s.accept()
 
-print "Listening on port" + str(PORT)
+# print "Listening on port" + str(PORT)
 
 
 
