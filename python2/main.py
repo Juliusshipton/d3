fd59fb59df69772126e2db0e5e3d01bf2cba1fd9
Julius Shipton <juliusshipton@gmail.com>
2023-07-24 15:58:11 +0800

    changed config

--- python2/main.py
+++ python2/main.py
@@ -20,7 +20,10 @@ print "Python 2 running ..."
 
 
 
-tagger=TimeTagger.TimeTagger('2138000XH1')
+# tagger=TimeTagger.TimeTagger('2138000XH1')
+
+tagger=TimeTagger.TimeTagger('1634000FWP')
+
 
 params = [1, 2, 3]
 
