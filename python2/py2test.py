366e934a81ec621259585f81f0c88c0e51819325
Julius Shipton <juliusshipton@gmail.com>
2023-07-25 17:15:39 +0800

    added python2 test

---
+++ python2/py2test.py
@@ -0,0 +1,14 @@
+
+
+print "Running Python2 .."
+
+from ..python3.hardware.nidaq import Scanner
+
+test = Scanner( CounterIn='/Dev1/Ctr1',
+					CounterOut='/Dev1/Ctr0',
+					TickSource='/Dev1/PFI3',
+					AOChannels='/Dev1/ao0:2',
+					x_range=(0.0,200.0),#100x
+					y_range=(0.0,200.0),
+					z_range=(0,100.0),
+					v_range=(-1.00,1.00))
