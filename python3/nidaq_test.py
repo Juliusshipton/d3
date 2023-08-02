bfb9a736516a9fbac83678f61e4c254cf1a65a74
Julius Shipton <juliusshipton@gmail.com>
2023-08-02 15:45:36 +0800

    ommitted test

--- python3/nidaq_test.py
+++ python3/nidaq_test.py
@@ -3,45 +3,45 @@ import nidaqmx
 
 print("Running python3 NIDAQ Test ...")
 
-# Pulse Train Counter Test
+# # Pulse Train Counter Test
 
-print("Creating Pulse Train Counter ...")
+# print("Creating Pulse Train Counter ...")
 
-DAQmx_Val_Low = ctypes.c_int32(10214)
+# DAQmx_Val_Low = ctypes.c_int32(10214)
 
-COTask = nidaqmx.Task()
+# COTask = nidaqmx.Task()
 
-print("Pulse Train Counter Created ...")
+# print("Pulse Train Counter Created ...")
 
-COTask.co_channels.add_co_pulse_chan_freq(
-    'Dev1/Ctr2', # Specifies the names of the counters to use to create the virtual channels. 
-    '', # Optional to assign to channel
-    units = nidaqmx.constants.FrequencyUnits.HZ, # Frequency unit
-    idle_state = DAQmx_Val_Low, # Specifies the resting state of the output terminal
-    initial_delay = ctypes.c_double(0), # Is the amount of time in seconds to wait before generating the first pulse.
-    freq = 1000.0, # Specifies at what frequency to generate pulses.
-    duty_cycle = 0.5 # Is the width of the pulse divided by the pulse period. NI-DAQmx uses this ratio combined with frequency to determine pulse width and the interval between pulses.
-    )
+# COTask.co_channels.add_co_pulse_chan_freq(
+#     'Dev1/Ctr2', # Specifies the names of the counters to use to create the virtual channels. 
+#     '', # Optional to assign to channel
+#     # units = nidaqmx.constants.FrequencyUnits.HZ, # Frequency unit
+#     idle_state = DAQmx_Val_Low, # Specifies the resting state of the output terminal
+#     initial_delay = ctypes.c_double(0), # Is the amount of time in seconds to wait before generating the first pulse.
+#     freq = 1000.0, # Specifies at what frequency to generate pulses.
+#     duty_cycle = 0.5 # Is the width of the pulse divided by the pulse period. NI-DAQmx uses this ratio combined with frequency to determine pulse width and the interval between pulses.
+#     )
 
-COTask.timing.cfg_samp_clk_timing(
-    source = 'OnboardClock',
-    rate = 10000.0, # Sample rate in samples per second
-    samps_per_chan = 10000, # Number of samples to acquire per channel
-    sample_mode = nidaqmx.constants.AcquisitionType.FINITE, # Continuous acquisition mode
-    )
+# COTask.timing.cfg_samp_clk_timing(
+#     source = '',
+#     rate = 10000.0, # Sample rate in samples per second
+#     samps_per_chan = 10000, # Number of samples to acquire per channel
+#     sample_mode = nidaqmx.constants.AcquisitionType.FINITE, # Continuous acquisition mode
+#     )
 
-COTask.out_stream.output_buf_size = 50
-COTask.timing.samp_clk_src = 'OnboardClock'
+# COTask.out_stream.output_buf_size = 50
+# COTask.timing.samp_clk_src = 'OnboardClock'
 
-print("Pulse Train Counter Configured ...")
+# print("Pulse Train Counter Configured ...")
 
-print("Starting Pulse Train Counter ...")
-COTask.start()
-COTask.wait_until_done()
+# print("Starting Pulse Train Counter ...")
+# COTask.start()
+# COTask.wait_until_done()
 
-print("Stoping Pulse Train Counter ...")
-COTask.stop()
-COTask.close()
+# print("Stoping Pulse Train Counter ...")
+# COTask.stop()
+# COTask.close()
 
 
 
